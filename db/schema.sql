6606a5c2016924e99a34346f78f388a776ddc75e
スティーブンさん <underyourcover@icloud.com>
2019-08-13 03:15:34 -0700

    updated schema to add photos_id

--- db/schema.sql
+++ db/schema.sql
@@ -6,10 +6,11 @@ USE airbnb;
 
 CREATE TABLE `photos` (
   `id` VARCHAR(16),
+  `photos_id` INT NOT NULL AUTO_INCREMENT,
   `likes` INT,
   `username` VARCHAR(64),
   `link` VARCHAR(255),
   `tag` VARCHAR(32),
   `photo_set` VARCHAR(10),
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`photos_id`)
 )
